52ff181ffd12dec5493d6c935a9ea5216f68cbfb
231312 <1878631986@qq.com>
2023-09-04 11:31:33 +0800

    Initial commit

--- src/main/java/com/news/controller/IndexController.java
+++ src/main/java/com/news/controller/IndexController.java
@@ -1,20 +1,33 @@
 package com.news.controller;
 
+import com.news.service.CategoryService;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+
+import javax.annotation.Resource;
 
 /**
  * @author 18786
  */
 @Controller
 public class IndexController {
+    @Resource
+    private CategoryService categoryService;
+
     @GetMapping("/admin/index")
-    public String index() {
+    public String adminIndex() {
         return "admin/index";
     }
 
     @GetMapping("/403")
     public String forbidden() {
         return "error/403";
+    }
+
+    @GetMapping("/index")
+    public String index(Model model) {
+        model.addAttribute("categories", categoryService.getCategoryList());
+        return "index";
     }
 }
